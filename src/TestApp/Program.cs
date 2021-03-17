495107ed575e7dcb372fc2a876e495ee5ce623e2
Stanko Culaja <stanko.culaja@kolotree.com>
2021-03-17 10:52:06 +0100

    Added test app

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using TimeZoneConverter;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var timeZoneInfo = TZConvert.GetTimeZoneInfo("Europe/Moscow");
+            Console.WriteLine(timeZoneInfo);
+            Console.WriteLine($"BaseUtcOffset: {timeZoneInfo.BaseUtcOffset}");
+            Console.WriteLine($"DTS offset: {timeZoneInfo.BaseUtcOffset + (timeZoneInfo.GetAdjustmentRules().FirstOrDefault()?.DaylightDelta ?? TimeSpan.Zero)}");
+        }
+    }
+}

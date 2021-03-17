d73a8e2195469a203cee37877eb8b70496d7c990
Stanko Culaja <stanko.culaja@kolotree.com>
2021-03-17 11:01:05 +0100

    Added test app

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -12,6 +12,8 @@ namespace TestApp
             Console.WriteLine(timeZoneInfo);
             Console.WriteLine($"BaseUtcOffset: {timeZoneInfo.BaseUtcOffset}");
             Console.WriteLine($"DTS offset: {timeZoneInfo.GetAdjustmentRules().FirstOrDefault()?.DaylightDelta.ToString() ?? "N/A"}");
+            Console.WriteLine($"Winter time: {TimeZoneInfo.ConvertTimeFromUtc(new DateTime(2021, 1, 1), timeZoneInfo)}");
+            Console.WriteLine($"Summer time: {TimeZoneInfo.ConvertTimeFromUtc(new DateTime(2021, 7, 1), timeZoneInfo)}");
         }
     }
 }
